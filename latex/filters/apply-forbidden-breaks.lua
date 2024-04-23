5ce6dc4cb6e5a464a4a0ea8c22548b09cefab872
Matthew Leffler <mattleff@gmail.com>
2024-04-23 09:59:07 +0700

    Regroup strings to match book names w/ numerics (#18)
    * Regroup strings to match book names w/ numerics
    
    * Update linting

--- latex/filters/apply-forbidden-breaks.lua
+++ latex/filters/apply-forbidden-breaks.lua
@@ -1,5 +1,7 @@
 -- local logging = require 'logging'
 
+traverse = 'topdown'
+
 function read_file_lines(path)
     local file = io.open(path, "rb")
     if not file then return nil end
@@ -14,12 +16,37 @@ function read_file_lines(path)
     return lines
 end
 
-traverse = 'topdown'
+local words = read_file_lines('./dictionaries/forbidden-breaks')
+
+function Inlines (inlines)
+    local new_inlines = {}
+    local current_string = ""
+
+    -- logging.temp('inlines', inlines)
+    for i = 1, #inlines do
+        if inlines[i].t == "Str" then
+            current_string = current_string .. inlines[i].text
+        elseif inlines[i].t == "Space" or inlines[i].t == "SoftBreak" then
+            current_string = current_string .. " "
+        else 
+            if current_string then
+                table.insert(new_inlines, pandoc.Str(current_string))
+                current_string = ""
+            end
+            table.insert(new_inlines, inlines[i])
+        end
+    end
+    if current_string then
+        table.insert(new_inlines, pandoc.Str(current_string))
+    end
+
+    -- logging.temp('new_inlines', new_inlines)
+    return new_inlines
+end
 
 function Str (el)
-    local words = read_file_lines('./dictionaries/forbidden-breaks')
-
     local t = {el.text}
+    -- logging.temp('text', el.text)
     for w = #words, 1, -1 do
         word = words[w]
         for i, textOrEl in ipairs(t) do
