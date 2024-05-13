99dbb91eb27f0f8219bf33fb90872c7810433a74
Matthew Leffler <mattleff@gmail.com>
2024-05-13 15:36:31 +0700

    Switch to hairline space

--- latex/filters/space-italics.lua
+++ latex/filters/space-italics.lua
@@ -1,6 +1,7 @@
 function Emph(el)
+    local space = ' ';
     local newContent = el.content
-    table.insert(newContent, 1, pandoc.Str(' '));
-    table.insert(newContent, pandoc.Str(' '));
+    table.insert(newContent, 1, pandoc.Str(space));
+    table.insert(newContent, pandoc.Str(space));
     return pandoc.Emph(newContent)
 end
