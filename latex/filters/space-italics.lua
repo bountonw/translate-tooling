58cbd0d4ccc125d70434980ae9e23eabc398002e
Matthew Leffler <mattleff@gmail.com>
2024-05-14 16:24:35 +0700

    Remove duplicate spaces

--- latex/filters/space-italics.lua
+++ latex/filters/space-italics.lua
@@ -1,7 +1,4 @@
 function Emph(el)
     local space = ' ';
-    local newContent = el.content
-    table.insert(newContent, 1, pandoc.Str(space));
-    table.insert(newContent, pandoc.Str(space));
-    return pandoc.Emph(newContent)
+    return {pandoc.Str(space), el, pandoc.Str(space)}
 end
