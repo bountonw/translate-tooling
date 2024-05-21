a5b4430159d44b876669562379e2f30779534b14
Matthew Leffler <mattleff@gmail.com>
2024-05-21 15:15:16 +0700

    Only replace duplicate normal spaces

--- latex/filters/remove-duplicate-spaces.lua
+++ latex/filters/remove-duplicate-spaces.lua
@@ -2,5 +2,5 @@ local utf8 = require 'lua-utf8'
 
 -- The Unicode character \u200a apparently isn't in the Lua space character class (%s)
 function Str(el)
-    return pandoc.Str(utf8.gsub(utf8.gsub(utf8.gsub(el.text, "  ", " "), "  ", " "), "%s+", " "))
+    return pandoc.Str(utf8.gsub(utf8.gsub(utf8.gsub(el.text, "  ", " "), "  ", " "), " +", " "))
 end
