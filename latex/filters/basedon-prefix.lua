3a5a70089f4074c795f59cf74fcc0f4fe3cb7dce
Matthew Leffler <mattleff@gmail.com>
2024-12-04 17:07:15 +0700

    Add space after basedon prefix (#70)
    * Add space after basedon prefix
    
    * Add lua filter to properly handle basedon prefix
    
    * Fix lint issue
    
    * Fix syntax error
    
    * Handle empty string

---
+++ latex/filters/basedon-prefix.lua
@@ -0,0 +1,15 @@
+-- local logging = require 'logging'
+local function basedon_prefix(m)
+    -- logging.temp('m', m.chapter.basedon)
+    if m.chapter.basedon ~= nil and m.chapter.basedon ~= "" then
+        if string.match(pandoc.utils.stringify(m.chapter.basedon), "^%d") then
+            table.insert(m.chapter.basedon, 1, pandoc.Str(' '))
+        end
+        table.insert(m.chapter.basedon, 1, pandoc.Str('ศึกษาควบคู่กับ'))
+    end
+    return m
+end
+
+return {{
+    Meta = basedon_prefix
+}}
