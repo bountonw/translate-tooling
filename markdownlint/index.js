e4c211bd568a9669609602e79c36bff47c529208
Brian Wilson <15809934+bountonw@users.noreply.github.com>
2025-07-18 14:38:01 +0700

    Update index.js to call new linting

--- markdownlint/index.js
+++ markdownlint/index.js
@@ -63,6 +63,19 @@ const CHECKS = [
     },
   },
   {
+    globPath: "./lo/**/03_public/",
+    customFormatting: "custom-formatting-lao-am-vowel.js",
+    config: {
+      default: false,
+    },
+  },
+  {
+    globPath: "./lo/assets/dictionaries/",
+    customFormatting: "custom-formatting-lao-am-vowel.js",
+    config: {
+      default: false,
+    },
+  {
     globPath: "./th/**/",
     customFormatting: "custom-formatting.th.js",
     config: {
