a774471712167b2749295ee7a83aaad455103871
Brian Wilson <15809934+bountonw@users.noreply.github.com>
2025-09-03 06:07:39 -0700

    Exclude test folders from markdown linting

--- markdownlint/index.js
+++ markdownlint/index.js
@@ -108,6 +108,7 @@ const CHECKS = [
       "!**/00_source/*.md",
       "!**/001_machineraw/*.md",
       "!**/README.md",
+      "!**/test/**/*.md",
       ...((negativeGlobPath && [`${negativeGlobPath}*.md`]) || []),
     ]);
     console.log(`Linting ${paths.length} files in "${globPath}"`);
